5c38ee8d3822ca698e49515a4f25873fd352efba
yassir <chakyassir7@gmail.com>
2025-10-28 04:12:33 +0100

    Refactor App component to manage FlashCards state and improve question/answer handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
 import { useState, useEffect, useRef } from 'react'
 import FlashCards from './components/FlashCards'
 import './App.css'
-import { use } from 'react';
 
 function App() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
-  const inputRef = useRef(null);
+  const [show, setShow] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  function handleSave() {
+    const newCard = {"question": question, "answer": answer};
+    setCards([...cards, newCard])
+    setQuestion("");
+    setAnswer("");
+  }
+  function handleShow() {
+    setShow(!show);
+  }
   
   return (
     <div>
       <label>Your Question: </label>
       <input 
         type="text" 
-        ref={inputRef}
         onChange={(e) => (setQuestion(e.target.value))}
+        value={question}
       />
       <br />
       <label>Value: </label>
       <input 
         type="text"
         onChange={(e) => (setAnswer(e.target.value))}
+        value={answer}
       />
       <br />
-      <FlashCards question={question} answer={answer} />
+      <button
+        onClick={handleSave}
+      >Add</button>
+      <button
+        onClick={handleShow}
+      >
+        {show ? "Hide" : "Show"}
+      </button>
+      <br />
+      {
+        show && cards.map((card, index) => (
+          <FlashCards 
+            key={index}
+            question={card.question} 
+            answer={card.answer} 
+          />
+        ))
+      }
     </div>
   )
 }
