6bd211c929eb0e33841d15efbbf32eec36659e82
yassir <chakyassir7@gmail.com>
2025-11-14 19:30:59 +0100

    Add form handling for user input; implement handleChange and handleSubmit functions

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ function App() {
   const [answer, setAnswer] = useState("");
   const [show, setShow] = useState(false);
   const [cards, setCards] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    age: '',
+    pw: '',
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...FormData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Data: ", formData);
+  }
 
   function hello(param) {
     alert(`Hello from ${param}`);
@@ -69,6 +88,22 @@ function App() {
       }
       <Increase />
       <Car carName="App" onCarClick={hello} />
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Name">Name: </label>
+        <input type="text" name='name' value={formData.name} onChange={handleChange}/>
+        <br />
+        <label htmlFor="Email">Email: </label>
+        <input type="text" name='email' value={formData.email} onChange={handleChange}/>
+        <br />
+        <label htmlFor="Email">Age: </label>
+        <input type="text" name='age' value={formData.age} onChange={handleChange}/>
+        <br />
+        <label htmlFor="Email">Password: </label>
+        <input type="text" name='pw' value={formData.pw} onChange={handleChange}/>
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
